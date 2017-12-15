e583fb5b73f38775b70428aacaaba15a187a2747
NickAk <asoman97@gmail.com>
2017-12-15 13:41:19 +0300

    Initial commit with scenes

---
+++ MyDragon/Assets/Scripts/Menu/MenuHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuHandler : MonoBehaviour {
+
+    public void StartButtonPressed()
+    {
+        SceneManager.LoadScene("game");
+    }
+
+}
