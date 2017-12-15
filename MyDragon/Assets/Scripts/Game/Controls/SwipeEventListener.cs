bb8b3bc07bce0113b869405c8b4c6af23243f471
NickAk <asoman97@gmail.com>
2017-12-15 16:41:24 +0300

    Lean touch included

---
+++ MyDragon/Assets/Scripts/Game/Controls/SwipeEventListener.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Lean.Touch;
+
+
+public class SwipeEventListener : MonoBehaviour
+{
+
+    public delegate void SwipeAction(int x, int y);
+    public static event SwipeAction Swipe;
+
+    void Start()
+    {
+        LeanTouch.OnFingerSwipe += OnFingerSwipe;
+    }
+
+    private void OnDisable()
+    {
+        LeanTouch.OnFingerSwipe -= OnFingerSwipe;
+    }
+    
+
+    private void OnFingerSwipe(LeanFinger finger)
+    {
+        var swipe = finger.SwipeScreenDelta;
+
+        if (swipe.x < -Mathf.Abs(swipe.y))
+        {
+            Swipe(-1, 0);
+        }
+
+        if (swipe.x > Mathf.Abs(swipe.y))
+        {
+            Swipe(1, 0);
+        }
+
+        if (swipe.y < -Mathf.Abs(swipe.x))
+        {
+            Swipe(0, -1);
+        }
+
+        if (swipe.y > Mathf.Abs(swipe.x))
+        {
+            Swipe(0, 1);
+        }
+    }
+}
---
+++ MyDragon/Assets/Scripts/Game/Player/PlayerMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    private void OnEnable()
+    {
+        SwipeEventListener.Swipe += DoMove;
+    }
+
+    private void OnDisable()
+    {
+        SwipeEventListener.Swipe -= DoMove;
+    }
+
+    private void DoMove(int x, int y)
+    {
+        Debug.Log(x + y);
+    }
+}
